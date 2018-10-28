9236d0d8d0377bce7d3bd4bdc8e2b5a32481c5ed
Francis Rodríguez <frc13elche@gmail.com>
2018-10-28 13:14:09 +0100

    añadidas definiciones

--- src/basicosfrancisrodriguez/Basicosfrancisrodriguez.java
+++ src/basicosfrancisrodriguez/Basicosfrancisrodriguez.java
@@ -15,7 +15,18 @@ public class Basicosfrancisrodriguez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        string();      // TODO code application logic here
     }
-    
+    public static void string() {
+        String string="La variable string sirve para introducir texto en el código, en vez de introducir un número o una función";
+        int longstr1= string.length();
+        System.out.println(string);
+        String string2="En las strings, podemos hacer varias cosas, como comprobar el número de carácteres de la string anterior si así lo deseamos";
+        System.out.println(string2);
+        System.out.println("Gracias a la función (variable).length, podemos saber que la primera string tiene " + longstr1 + " carácteres");
+        
+        
+        
+        
+    }
 }
